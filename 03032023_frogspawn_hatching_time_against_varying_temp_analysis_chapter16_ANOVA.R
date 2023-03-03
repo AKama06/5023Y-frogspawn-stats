8ab0292d53f3d6b514a89b2b5ed4e1c1dc5f684f
AKama06 <Abioseh.Kamara@uea.ac.uk>
2023-03-03 03:30:20 +0000

    Added hypothesis, renames column names, pivoted table from wider to longer, and removed NA values.

--- 03032023_frogspawn_hatching_time_against_varying_temp_analysis_chapter16_ANOVA.R
+++ 03032023_frogspawn_hatching_time_against_varying_temp_analysis_chapter16_ANOVA.R
@@ -1,8 +1,10 @@
 #___________________________----
 # SET UP ----
-## An analysis of frogspawn hatching time with varying terperatures of 13°C, 18°C and 25°C. ----
+## A statistical analysis of frogspawn hatching time with varying terperatures of 13°C, 18°C and 25°C. ----
 
-### Newly-layed frogspawn were collected from a pond in the Italian Alps, brought back to the lab, and divided into 60 water containers.20 of the containers’ water temperature was kept to 13°C, 20 containers were kept to 18°C and the remaining 20 containers were kept to 25°C. Water containers were monitored and days until hatching of egg (hatching times) were recorded.  ------
+### Newly-layed frogspawn were collected from a pond in the Italian Alps, brought back to the lab, and divided into 60 water containers.20 of the containers’ water temperature was kept to 13°C, 20 containers were kept to 18°C and the remaining 20 containers were kept to 25°C. Water containers were monitored and days until hatching of egg (hatching times) were recorded.  ----
+
+### Hypothesis- mean frogspawn hatching time will vary with temperature level ----
 #__________________________----
 # PACKAGES ----
 library(tidyverse)
@@ -19,12 +21,21 @@ colnames(frogspawn)
 #rename column names
 frogspawn <- rename(frogspawn,
                     "id"="frogspawn_sample_id",
-                    "temp_13"="temperature13",
-                    "temp_18"="temperature18",
-                    "temp_25"="temperature25")
+                    "13"="temperature13",
+                    "18"="temperature18",
+                    "25"="temperature25")
 # Get a sum of how many observations are missing in our dataframe
 frogspawn %>% 
   is.na() %>% 
   sum()
 summary(frogspawn)
+glimpse(frogspawn)
 
+#pivot table from wider to longer
+frogspawn <- pivot_longer(data = frogspawn,
+                          cols = "13":"25",
+                          names_to = "temperature",
+                          names_prefix = "temp",
+                          values_to = "days")
+frogspawn <- drop_na(frogspawn)
+frogspawn
